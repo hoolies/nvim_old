14f6dfbc49e94dd44de9e15975c522da65b0417b
Chrysanthos Rouvellas <75946785+hoolies@users.noreply.github.com>
2023-05-28 21:44:31 -0400

    Delete lua/colors directory

--- lua/colors/init.lua
+++
@@ -1,8 +0,0 @@
-require('tokyonight')._load('Moon')
---vim.cmd("colorscheme onedarkpro")
-
--- Highlights
-vim.cmd[[highlight LineNr guifg=#29465B]]               -- The Numbers on the left
-vim.cmd[[highlight CursorColumn guifg=#737CA1]]         -- The cursorcolumn color
-vim.cmd[[highlight CursorLine guifg=#737CA1]]           -- The cursorline color
-vim.cmd[[highlight CursorLineNr guifg=#C45AEC]]         -- The number of actual line
